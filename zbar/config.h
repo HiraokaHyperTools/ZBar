8bb3ee8c23608cc7ef5fddd1b9630e1d30bcac24
KU <ku@digitaldolphins.jp>
2021-06-01 17:12:39 +0900

    0.110

--- zbar/config.h
+++ zbar/config.h
@@ -166,19 +166,19 @@
 #define PACKAGE_NAME "zbar"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "zbar 0.109"
+#define PACKAGE_STRING "zbar 0.110"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "zbar"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.109"
+#define PACKAGE_VERSION "0.110"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.109"
+#define VERSION "0.110"
 
 /* Define to 1 if the X Window System is missing or not being used. */
 #define X_DISPLAY_MISSING 1
